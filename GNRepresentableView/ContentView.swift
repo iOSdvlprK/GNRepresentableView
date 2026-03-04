97d10d497c5d9b504aa968d9cfb9b4c7a904e5b0
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-03-04 21:22:52 +0900

    Initial Commit

---
+++ GNRepresentableView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GNRepresentableView
+//
+//  Created by joe on 3/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GNRepresentableView/GNRepresentableViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  GNRepresentableViewApp.swift
+//  GNRepresentableView
+//
+//  Created by joe on 3/4/26.
+//
+
+import SwiftUI
+
+@main
+struct GNRepresentableViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
