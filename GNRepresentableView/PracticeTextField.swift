cfb8f691c271c585c6cbc52d45294ebf4843c288
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-03-06 21:56:13 +0900

    restrict PracticeTextField input to numbers
    - set the UITextField delegate via a SwiftUI coordinator and add a Coordinator class that implements UITextFieldDelegate

--- GNRepresentableView/PracticeTextField.swift
+++ GNRepresentableView/PracticeTextField.swift
@@ -11,11 +11,25 @@ struct PracticeTextField: UIViewRepresentable {
     func makeUIView(context: Context) -> some UIView {
         let textField = UITextField()
         textField.borderStyle = .line
+        textField.delegate = context.coordinator
         textField.setContentHuggingPriority(.defaultHigh, for: .vertical)
         return textField
     }
     
     func updateUIView(_ uiView: UIViewType, context: Context) {
         
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator()
+    }
+    
+    class Coordinator: NSObject, UITextFieldDelegate {
+        func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+            if string.isEmpty {
+                return true
+            }
+            return Int(string) != nil
+        }
     }
 }
